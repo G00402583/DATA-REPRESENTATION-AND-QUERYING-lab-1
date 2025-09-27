ffd37b2458979d5c9312da1060110aefb6754481
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-09-27 19:28:04 +0100

    2b.js

--- 2b.js
+++ 2b.js
@@ -1,6 +1,8 @@
+// Define an arrow function that takes one parameter: myName
 let myVar = (myName)=>{
+    // Log a welcome message with the provided name
     console.log("Welcome to Data Representation and Query" +myName);
 
 }
-
+// Call the function with the argument "Maliq"
     myVar("Maliq");
