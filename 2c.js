fa15ec6c0e10097b06a3cdcc6ff3e5f94dbb7024
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-09-27 19:33:45 +0100

    2c.js

--- 2c.js
+++ 2c.js
@@ -1,7 +1,12 @@
+// Define an arrow function that takes two parameters: x and y
 let myNum = (x, y)=>{
+    // Add x and y, store the result in z
     let z = x+y;
+    // Log the result to the console
+
     console.log(z);
 
 }
 
+// Call the function with arguments 2 and 3
 myNum(2,3);
