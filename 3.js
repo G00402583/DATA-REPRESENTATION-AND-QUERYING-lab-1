a13b3af7231389db62557d020e53ce066a560b1c
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-09-27 19:31:19 +0100

    3.js

--- 3.js
+++ 3.js
@@ -1,18 +1,21 @@
+// Initialize an empty array to store tasks
 let Tasks = [];
 
+// Initialize an empty array to store tasks
 let addTask = (task)=>{
     Tasks.push(task);
     console.log(task + " has been added to my Tasks!");
     console.log("Number of task in my Tasks list is" + Tasks.length);
 }
 
-
+// Function to list all tasks
 let listAllTasks = ()=>{
     
   //  for(let i=0;i<Tasks.length;i++){
    //     console.log(Tasks[i]);
   //  }
 
+    // Loop through each task and print it
 Tasks.forEach(
     (task)=>{
         console.log(task);
@@ -20,12 +23,12 @@ Tasks.forEach(
 )
 }
 
-
-let deleteTasks = (task)=>{
-    let index = Tasks.indexOf(task);
+// Function to delete a task from the list
+let deleteTasks = (task)=>{ 
+    let index = Tasks.indexOf(task); // Find the index of the task
     if(index > -1){
     
-    Tasks.splice(index,1);
+    Tasks.splice(index,1);    // Remove the task if found
     console.log(task + "task has benn removed");
     }else {
         console.log(task + " work was not found");
@@ -34,7 +37,7 @@ let deleteTasks = (task)=>{
 
 }
 
-
+// Test the functions
 addTask("Work");
 addTask("Eat");
 listAllTasks();
