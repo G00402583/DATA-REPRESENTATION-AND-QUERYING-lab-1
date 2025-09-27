a8ad7d8e35a08168218e87dceb2cd2e85cfccdc4
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-09-27 19:26:53 +0100

    2a.js

--- 2a.js
+++ 2a.js
@@ -1,6 +1,9 @@
+// Define an arrow function named myVar
 let myVar = ()=>{
+     // Log a message to the console
     console.log("Data Representation Query");
 
 }
 
+// Call the function
     myVar();
