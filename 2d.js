8c23d6e897f78906b158491c0907fc06cdc4bac4
G00402583 <92363325+G00402583@users.noreply.github.com>
2025-09-27 19:32:33 +0100

    2d.js

--- 2d.js
+++ 2d.js
@@ -1,10 +1,15 @@
+// Create an array of ages
 const ages =[25,31,42,77];
 
+
+// Use the map() method to loop through each element in the array
 let myArray = ages.map(
     (element)=>{
+        // If the element is greater than 70, print double the value
         if(element > 70){
             console.log(element*2);
         }else{
+            // Otherwise, just print the element
             console.log(element);
         }
     }
